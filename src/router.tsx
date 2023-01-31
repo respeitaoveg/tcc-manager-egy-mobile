cb911bdb9a859a60f8c34a9552a89f9f5ba097a4
Yuri Braga <yuri.moc.rb@gmail.com>
2023-01-30 22:49:23 -0300

    feat: add login page

--- src/router.tsx
+++ src/router.tsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter } from 'react-router-dom'
 import App from './App'
+import Login from './pages/login'
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <App />
+    element: <App />,
+  },
+  {
+    path: '/login',
+    element: <Login />
   }
 ])
 
