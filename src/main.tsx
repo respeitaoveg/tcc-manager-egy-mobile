20fc1a8c92fe652f6379c31218f1f5775bde217a
Yuri Braga <yuri.moc.rb@gmail.com>
2023-02-08 00:36:18 -0300

    feat: add cart provider in main

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,16 @@ import router from './router'
 import { ChakraProvider } from '@chakra-ui/react'
 import { RouterProvider } from "react-router-dom"
 import AuthProvider from './contexts/AuthContext'
+import CartProvider from './contexts/CartContext'
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider>
       <AuthProvider>
-        <RouterProvider router={router} />
+        <CartProvider>
+          <RouterProvider router={router} />
+        </CartProvider>
       </AuthProvider>
     </ChakraProvider>
   </React.StrictMode>
