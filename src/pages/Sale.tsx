94db5b52549234fa2040f35b7ac254170f822a35
Yuri Braga <yuri.moc.rb@gmail.com>
2023-02-20 16:04:49 -0300

    feat: add circular progress

--- src/pages/Sale.tsx
+++ src/pages/Sale.tsx
@@ -1,3 +1,16 @@
+import { CircularProgress, CircularProgressLabel, Flex, useColorModeValue } from "@chakra-ui/react";
+
 export default function Sale() {
-  return <div>sale</div>
+  return (
+    <Flex
+      minH={'calc(100vh - 200px)'}
+      align={'center'}
+      justify={'center'}
+      bg={useColorModeValue('gray.50', 'gray.800')}
+    >
+      <CircularProgress isIndeterminate color='teal' size={20}>
+        <CircularProgressLabel fontSize={12}>40%</CircularProgressLabel>
+      </CircularProgress>
+    </Flex>
+  )
 }
