289e1adc97df0b8d2c25d45194386b663712862a
Yuri Braga <yuri.moc.rb@gmail.com>
2023-02-25 17:09:53 -0300

    feat: create validates to cpf and cnpj

---
+++ src/utils/validateCpfCnpj.ts
@@ -0,0 +1,9 @@
+export const cpfFormat = '999.999.999-99'
+export const cpfFormatHash = '###.###.###-##'
+export const cpfRegex = /^\d{3}\.?\d{3}\.?\d{3}-?\d{2}$/
+
+export const cnpjFormat = '99.999.999/9999-99'
+export const cnpjFormatHash = '##.###.###/####-##'
+export const cnpjRegex = /^\d{2}\.\d{3}\.\d{3}\/\d{4}-\d{2}$/
+
+export const cpfCnpjRegex = /^\d{3}\.?\d{3}\.?\d{3}-?\d{2}|\d{2}\.?\d{3}\.?\d{3}\/?\d{4}-?\d{2}$/
